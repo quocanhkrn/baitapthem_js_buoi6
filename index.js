f872d8289de1aabee4572320698ed57a149d34a4
DANG QUOC ANH <quocanh.krongno@gmail.com>
2022-10-28 13:28:19 +0700

    update

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ function ex() {
   let num = parseInt(document.getElementById("num").value)
   let result = document.getElementById("exResult")
   let numString = "2 "
+  loop01:
   for (let i = 3; i <= num; i++) {
-    let check = true
+    loop02:
     for (let j = 2; j < i; j++) {
-      if (i % j === 0) check = false
+      if (i % j === 0) continue loop01
     }
-    if (check) {
-      numString += `${i} `
-    }
+    numString += `${i} `
   }
   result.classList.add("alert")
   result.innerHTML = `${numString}`
